9ca9e6001965b3e5ff06f97473ed3a2d3b6fb0ea
Tom Godfrey <thomas.godfrey@nhs.net>
2019-05-29 11:46:05 +0100

    Add 'source' function call.

--- 5_MI_FOBT_FIT_comparison.R
+++ 5_MI_FOBT_FIT_comparison.R
@@ -29,70 +29,65 @@
 ### Step 1: Housekeeping
 
 ## Load packages
-install.packages('dplyr')
+library(dplyr)
 library(readr)
 library(haven)
 library(janitor)
 library(magrittr)
-library(dplyr)
-library(tidyr)
 library(lubridate)
 library(invgamma)
+library(here)
+library(tidyr)
+library("rlang", lib.loc="~/R/x86_64-redhat-linux-gnu-library/3.2")
 
 ## Define functions
 
 
 ## Set filepaths and import reporting period dates from Script 0
 # Define location of combined_extract_all and analsysis_dataset
-#source(here::here("code", "0_housekeeping.R"))
-
+source(here::here("code", "0_housekeeping.R"))
 
 
 ### Step 2: Import data
 #Keep this code and delete below when 'here' works
-#raw_db <- read_sav(sbsdb_path)
+sbsp_analysis_db <- read_sav(analysis_db_path) %>%
+  clean_names()
 
-############################################################################
-## Temp
-# currently use direct import since 'here' not working. Replace later.
-sbsdb_path  <- read_sav(paste0("/PHI_conf/CancerGroup1/Topics/BowelScreening/Data/",
-                               "Programme/2018-11/combined_extract_all.zsav"))
+analysis_db <- read_sav(paste0("/PHI_conf/CancerGroup1/Topics/BowelScreening/",
+                               "TPP/KPIs/Code + DB/TPP/data/analysis_dataset.rds"))
 
-
-analysis_db <- readRDS(paste0("/PHI_conf/CancerGroup1/Topics/BowelScreening/",
-                              "TPP/KPIs/Code + DB/TPP/data/analysis_dataset.rds"))
-# Directly define dates but delete later.
-date_first <- "2016-05-01"
-date_last  <- "2018-04-30"
-############################################################################
 
 ### STEP 3: Create Screening History file.
 
 # Use Select statement to keep only required variables: chinum screres date_round.
-slim_db1 <- clean_names(sbsdb_path) %>% 
-  select('chinum', 'screres', 'date_round')
-dim(slim_db1)
-names(slim_db1)
-
-
 # Sort cases by chi-number
-slim_db1 <- arrange(slim_db1, chinum)
-
 # Spread individual patients screening results onto single rows by screening round
-screening_history_byCHI <- slim_db1 %>% group_by(chinum) %>% spread(key = 'date_round',value = 'screres')
+screening_history_byCHI <- sbspdb_combined_extract_all %>% 
+  select('chinum', 'screres', 'date_round') %>%
+  arrange(chinum) %>%
+  group_by(chinum) %>%
+  spread(key = 'date_round',value = 'screres') %>%
+  ungroup()
+
 
 
 ### Step 4: Get relevant records for the KPI report.
-sbsdb_path1 <- clean_names(sbsdb_path)
-
-slim_db2 <- filter(sbsdb_path1,invdate >= as.Date(date_first) & invdate <= as.Date(date_last)) %>%
+#Open the most recent combined_extract_all file.
+sbsp_slimdb <- sbspdb_combined_extract_all %>%
+  filter(invdate >= as.Date(date_first) & invdate <= as.Date(date_last)) %>%
   filter(optin == 0) %>%
-  filter(hbr14 %in% 1:14)
-
-?as.Date
-
-dim(slim_db2)
-head(slim_db2)
+  filter(hbr14 %in% 1:14) %>%
+  filter(screres %in% c(1:18,21,22,24))
+dim(sbsp_slimdb)
+names(sbsp_slimdb)
 
 
+#Select report time period and comparison time period
+test_comp_db <- sbsp_slimdb %>% 
+  mutate(
+    fobt_flag = ifelse((invdate >= as.Date("2016-11-20") & invdate <= as.Date("2017-04-30")),1,0),
+    fit_flag  = ifelse((invdate >= as.Date("2017-11-20") & invdate <= as.Date("2018-04-30")),1,0)) %>%
+  filter(fobt_flag  == 1 | fit_flag == 1)
+
+#Create variables required.
 
